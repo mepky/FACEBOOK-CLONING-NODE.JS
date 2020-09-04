14eda7d3e5b9b927b2a9bf1b31d627b707e07618
mepky <mrcoder215@gmail.com>
2020-09-04 07:04:23 +0530

    post is created

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+const homeController = require('../controllers/home_controller');
+
+console.log('router loaded');
+
+
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
+
+// for any further routes, access from here
+// router.use('/routerName', require('./routerfile));
+
+
+module.exports = router;
