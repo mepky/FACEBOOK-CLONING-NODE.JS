9edb8a66f33e28c3b4a0f7f5b6c5ac83cd59ca6f
mepky <mrcoder215@gmail.com>
2020-09-04 20:49:34 +0530

    comments has added to db

--- models/comments.js
+++ models/comments.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-const postSchema = new mongoose.Schema({
+const commentSchema = new mongoose.Schema({
     content: {
         type: String,
         required: true
@@ -11,13 +11,13 @@ const postSchema = new mongoose.Schema({
         ref: 'User'
 
     },
-    comments: [{
+    post: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'comment'
-    }]
+        ref: 'post'
+    }
 }, {
     timestamps: true
 });
 
-const Post = mongoose.model('Post', postSchema);
-module.exports = Post;
+const Comment = mongoose.model('Comment', commentSchema);
+module.exports = Comment;
