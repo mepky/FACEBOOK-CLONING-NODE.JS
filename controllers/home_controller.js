75a6ed0a2c80980b643121d780716194a6b9356a
mepky <mrcoder215@gmail.com>
2020-09-04 19:47:08 +0530

    post is not displayed

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,23 @@
-module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
-    });
+const Post = require('../models/post');
+
+module.exports.home = function(req, res) {
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+    Post.findById({}, function(err, posts) {
+        if (err) {
+            console.log('Error in finding post');
+            return;
+        }
+
+        return res.render('home', {
+            title: "Home",
+            posts: posts
+        });
+
+    })
+
+
+
 }
 
 // module.exports.actionName = function(req, res){}
