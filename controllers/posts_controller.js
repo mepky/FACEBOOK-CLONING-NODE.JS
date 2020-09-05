858be234efa3fef4aba83abdc9252f4ae2f2a73d
mepky <mrcoder215@gmail.com>
2020-09-05 09:50:58 +0530

    comment delete added

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,12 +1,28 @@
 const Post = require('../models/post')
-
-module.exports.create = function(req, res){
+const Comment = require('../models/comments');
+module.exports.create = function(req, res) {
     Post.create({
         content: req.body.content,
         user: req.user._id
-    }, function(err, post){
-        if(err){console.log('error in creating a post'); return;}
+    }, function(err, post) {
+        if (err) { console.log('error in creating a post'); return; }
 
         return res.redirect('back');
     });
+}
+
+
+module.exports.destroy = function(req, res) {
+    Post.findById(req.params.id, function(err, post) {
+        // .id means converting the object id into string
+        if (post.user == req.user.id) {
+            post.remove();
+            Comment.deleteMany({ post: req.params.id }, function(err) {
+                return res.redirect('back');
+            });
+
+        } else {
+            return res.redirect('back');
+        }
+    })
 }
