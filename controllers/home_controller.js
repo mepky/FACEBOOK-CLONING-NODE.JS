e3b940de225584b14f41d263b348b391f971806c
mepky <mrcoder215@gmail.com>
2020-09-05 09:17:44 +0530

    displayed comment -m

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,19 +3,20 @@ const Post = require('../models/post');
 module.exports.home = function(req, res) {
         // console.log(req.cookies);
         // res.cookie('user_id', 25);
-        Post.find({}).populate('user').
-        populate({
-            path: 'comments',
-            populate: {
-                path: 'user'
-            }
-        }).exec(function(err, posts) {
-            return res.render('home', {
-                title: "Home",
-                posts: posts
-            });
+        Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'user'
+                }
+            }).exec(function(err, posts) {
+                return res.render('home', {
+                    title: "Home",
+                    posts: posts
+                });
 
-        });
+            });
 
 
 
